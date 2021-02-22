1f9c021aa083f199644ba4297b6656bc4b17e4ee
Wagner Torres <wstorres@gmail.com>
2021-02-22 15:11:42 -0300

    atualização do surefire e ajuste na String de data válida futura

--- src/test/java/tasks/functional/TasksTest.java
+++ src/test/java/tasks/functional/TasksTest.java
@@ -3,9 +3,12 @@ package tasks.functional;
 import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class TasksTest {
@@ -13,17 +16,21 @@ public class TasksTest {
     private WebDriver driver;
 
     @Before
-    public void setup(){
-        this.driver = new FirefoxDriver();
-        driver.navigate().to("http://localhost:8001/tasks/");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    public void setup() throws MalformedURLException {
+        DesiredCapabilities cap = DesiredCapabilities.firefox();
+        FirefoxOptions options = new FirefoxOptions();
+        cap.merge(options);
+        options.setHeadless(true);
+        this.driver = new RemoteWebDriver(new URL("http://192.168.15.9:4444/wd/hub"), cap);
+        driver.navigate().to("http://192.168.15.9:8001/tasks/");
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @Test
     public void testeDeveSalvarTarefaComSucesso(){
         driver.findElement(By.id("addTodo")).click();
         driver.findElement(By.id("task")).sendKeys("Tarefa 2021");
-        driver.findElement(By.id("dueDate")).sendKeys("18/02/2021");
+        driver.findElement(By.id("dueDate")).sendKeys("01/07/2021");
         driver.findElement(By.id("saveButton")).click();
         String mensagemCapturada = driver.findElement(By.id("message")).getText();
         Assert.assertEquals("Success!", mensagemCapturada);
